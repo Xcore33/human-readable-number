bbf40b2061a454e5a7e19123a793ac4a9026a356
xcore33 <mauler1987@gmail.com>
2020-03-08 13:34:40 +0300

    feat: add solution human readable number

--- src/index.js
+++ src/index.js
@@ -1,3 +1,57 @@
-module.exports = function toReadable (number) {
-  
-}
+module.exports = function toReadable(number) {
+
+    const numeral = [
+        "",
+        "one",
+        "two",
+        "three",
+        "four",
+        "five",
+        "six",
+        "seven",
+        "eight",
+        "nine",
+        "ten",
+        "eleven",
+        "twelve",
+        "thirteen",
+        "fourteen",
+        "fifteen",
+        "sixteen",
+        "seventeen",
+        "eighteen",
+        "nineteen",
+        "twenty"
+    ];
+
+    const dozens = [
+        "",
+        "",
+        "twenty",
+        "thirty",
+        "forty",
+        "fifty",
+        "sixty",
+        "seventy",
+        "eighty",
+        "ninety"
+    ];
+
+    let readableString = '';
+    
+    if (number == 0) {
+        readableString = 'zero';
+    }
+
+    let arrayFromNumber = number.toString().split("");
+
+    if (number <= 20 && number > 0 ) {
+        readableString = numeral[number];
+    } else if (number >= 21 && number < 100) {
+        readableString = dozens[arrayFromNumber[0]] + " " + numeral[arrayFromNumber[1]]
+    } else if (number > 99 && number < 1000) {
+        readableString = numeral[arrayFromNumber[0]] + " hundred " + (number % 100 == 0 ? "" : toReadable(number % 100));
+    }
+
+    return readableString.trim();
+};
